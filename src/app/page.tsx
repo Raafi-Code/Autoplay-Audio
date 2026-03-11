28823afa399b3011cfd8fa04d3021ce8a95dd606
Muhammad Rafi <m.raafi.official@gmail.com>
2026-03-11 14:07:56 +0800

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
   const wakeLockRef = useRef<any>(null);
 
   // Default play time (bisa diatur sesuai kebutuhan)
-  const defaultPlayTime = "14:00";
+  const defaultPlayTime = "14:10";
 
   // Keep screen awake - Request Wake Lock
   useEffect(() => {
